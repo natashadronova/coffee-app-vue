214d48f24996478e4107daca72a701c9718a00f8
Natalia Dronova <ndronova04@gmail.com>
2019-07-25 19:34:39 +1000

    Navbar added

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+     orders: [
+       { title:'Ninja Brew', slug: 'ninja-brew', ingredients:['banans', 'coffee','milk'] }
+     ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-wrapper brown lighten-3">
+      <div class="container">
+        <span class="brand-logo">Coffee App</span>
+        <ul class="right">
+          <li>
+            <router-link to=''>Home</router-link>
+          </li>
+          <li>
+            <router-link to=''>Sign In</router-link>
+          </li>
+          <li>
+            <router-link to=''>Sign Up</router-link>
+          </li>
+          <li>
+            <router-link to=''>
+            <a href="#" class=" btn-large  pink btn-flat"><i class="material-icons">add</i></a>
+          </router-link>
+          </li>
+        </ul>
+       
+      </div>
+
+    
+    </nav>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
