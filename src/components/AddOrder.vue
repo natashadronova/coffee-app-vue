3624317c29fd4a52c005663f2bea22a03a982c03
Natalia Dronova <ndronova04@gmail.com>
2019-08-23 15:31:22 +1000

    order form completed

--- src/components/AddOrder.vue
+++ src/components/AddOrder.vue
@@ -1,19 +1,83 @@
 <template>
-  <h2>Add Smoothie</h2>
+  <div class="add-coffee container">
+    <h2 class="center-align indigo-text">Order Coffee</h2>
+
+    <div class="row">
+      <form class="col s12">
+        <div class="row"></div>
+        <div class="input-field col s12">
+          <select>
+            <option value disabled selected>Coffee</option>
+            <option value="Latte">Latte</option>
+            <option value="Mocha">Mocha</option>
+            <option value="Hot Chocolate">Hot Chocolate</option>
+          </select>
+        </div>
+
+        <div class="input-field col s12">
+          <select>
+            <option value disabled selected>Size</option>
+            <option value="Small">Small</option>
+            <option value="Medium" default>Medium</option>
+            <option value="Large">Large</option>
+          </select>
+        </div>
+
+        <div class="input-field col s12">
+          <select multiple>
+            <option value disabled selected>Extras</option>
+            <optgroup label="Toppings">
+              <option value="Vanilla">Vanilla</option>
+              <option value="Hazelnut">Hazelnut</option>
+              <option value="Caramel">Caramel</option>
+            </optgroup>
+            <optgroup label="Milk">
+              <option value="Soy">Soy Milk</option>
+              <option value="Almond">Almond Milk</option>
+            </optgroup>
+            <optgroup label="Strength">
+              <option value="Decaf">Decaf</option>
+              <option value="Extra Shot">Extra Shot</option>
+            </optgroup>
+          </select>
+        </div>
+
+        <div class="input-field col s12">
+          <input
+            placeholder="If not coffee, then what?"
+            id="first_name"
+            type="text"
+            class="validate"
+          />
+          <!-- <label for="first_name">Other:</label> -->
+        </div>
+
+        <div class="input-field col s12">
+          <label>
+            <input type="checkbox" class="filled-in" />
+            <span>Save as default order?</span>
+          </label>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'AddOrder',
-  data(){
-    return {
+// import $ from 'jquery';
+import M from "materialize-css";
 
-    }
+export default {
+  name: "AddOrder",
+  data() {
+    return {};
+  },
+  mounted() {
+    M.AutoInit();
   }
-}
+};
 </script>
 
 <style>
-
 </style>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
